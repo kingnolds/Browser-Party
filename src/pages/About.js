ee282bae726bd8fe2816b63da9d1016f02bf5869
Spencer Nelson <srn314@gmail.com>
2022-03-04 13:58:08 -0800

    updating pages and adding navbar with container

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+        <div>
+      <h1>Contributors</h1>
+      <p>
+        Henry
+        Reid
+        Ronnel
+        Spencer
+      </p>
+      </div>
+        <div>
+      <h1>Game Info</h1>
+      <p>
+        Snake
+        Tic Tac Toe
+        Memory
+        Spot the Difference
+      </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div>
+            <button type='button'>Edit Profile</button>
+            <div>
+                <h1>Wins</h1>
+                <p>
+                    Total Wins: 0
+                </p>
+            </div>
+            <div>
+                <h1>Game Stats</h1>
+                <p>
+                    Round Wins: 0
+                </p>
+            </div>
+            <div>
+                <h1>Friends List</h1>
+                <p>
+                    All your friends.
+                </p>
+            </div>
+        </div>
+    );
+}
