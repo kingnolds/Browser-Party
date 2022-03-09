34f78ca68abb27f9af9a10f791e9af06cc5e491c
Reid Somers Flamm <reidsomersflamm@gmail.com>
2022-03-09 14:17:03 -0800

    style changes and login setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import Home from "../pages/Home";
 import Play from "../pages/Play";
 import Profile from "../pages/Profile";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
+import Register from "../pages/Register";
 import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <div>
-      <div className="NavBar">
-        <h4>Navbar</h4>
-        <Link to="/">Home</Link>
-        <Link to="/play">Play</Link>
-        <Link to="/profile">Profile</Link>
-        {props.userEmail ? (
+      <div className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand"><h4>Navbar</h4></a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link"><Link to="/">Home</Link></a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link"><Link to="/play">Play</Link></a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link"><Link to="/profile">Profile</Link></a>
+            </li>
+            <li className="nav-item">
+            <a className="nav-link"><Link to="/register">Register</Link></a>
+            </li>
+          </ul>
+        {/* {props.userEmail ? (
           <div>
             <h2>Welcome to the club, {props.userEmail}</h2>
             <button onClick={props.logMeOut}>LogOut</button>
@@ -31,8 +44,11 @@ export default function Navbar(props) {
               name="password"
             />
             <button>Login</button>
+            <button> Create Account</button>
           </form>
-        )}
+        )} */}
+        </div>
+
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,68 @@
 import React, { useState } from 'react';
 import {useParams} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-export default function Home() {
+export default function Home(props) {
+
+  let navigate = useNavigate(); 
+  const loginChange = () =>{ 
+    let path = `/login`; 
+    navigate(path);
+  }
+  const registerChange = () =>{ 
+    let path = `/register`; 
+    navigate(path);
+  }
+
+  const styles = {
+    button:
+    {
+
+    },
+    container:
+    {
+      // background: 'black',
+    },
+  }
 
   const params = useParams();
     return (
       <div>
-        <h1>Home</h1>
-      </div>
+
+          {/* {props.userEmail ? (
+          <div>
+            <h2>Welcome to the club, {props.userEmail}</h2>
+            <button onClick={props.logMeOut}>LogOut</button>
+          </div>
+        ) : (
+          <form onSubmit={props.logMeIn}>
+            <input
+              value={props.loginInfo.email}
+              onChange={props.handleInputChange}
+              name="email"
+            />
+            <input
+              value={props.loginInfo.password}
+              onChange={props.handleInputChange}
+              name="password"
+            />
+
+          </form>
+        )} */}
+          <div style={styles.container} div className="container">
+            <div className="row">
+              <div className="col text-center">
+              <h1>Home</h1>
+              <div>
+              <button type="submit" className="btn btn-primary btn-block" onClick={loginChange}>Login</button>
+            </div>
+            <br></br>
+            <div>
+              <button type="submit" className="btn btn-primary btn-block" onClick={registerChange}>Create Account</button>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
