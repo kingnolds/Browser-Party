208100c67169af479384ccfcd676805a11b5f7f4
Henry Weigand <henryweigand10@gmail.com>
2022-03-15 13:58:39 -0700

    chat

--- src/pages/Pregame.js
+++ src/pages/Pregame.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react';
 
 function Pregame({socket, room, players, username, isHost, checkbox, startGame}) {
     
-    const [messages, setMessages] = useState([])
+    const [messages, setMessages] = useState([{}])
+    const [newMessage, setNewMessage] = useState("")
 
     const styles = {
         card: {
@@ -16,69 +17,63 @@ function Pregame({socket, room, players, username, isHost, checkbox, startGame})
         }
       }
 
-    //   const sendMessage = () => {
-    //     var message = $inputMessage.val();
-    //     // Prevent markup from being injected into the message
-    //     message = cleanInput(message);
-    //     // if there is a non-empty message and a socket connection
-    //     if (message && connected) {
-    //       $inputMessage.val('');
-    //       addChatMessage({
-    //         username: username,
-    //         message: message
-    //       });
-    //       // tell server to execute 'new message' and send along one parameter
-    //       socket.emit('new message', message);
-    //     }
-    //   }
+    socket.on('add-new-message', function (data) {
+        setMessages(messages.push(data));
+    });
 
-    //   const addChatMessage = (data, options) => {
+    const sendMessage = () => {
+        socket.emit('send-new-message', username, newMessage, room);
+    }
 
-    //   }
-
-    //   socket.on('new message', function (data) {
-    //     addChatMessage(data);
-    //   });
-
+    const handleFormChange = (e) => {
+        setNewMessage(e.target.value)
+    }
 
     return (
         <div>
             <div>
-                        <h1>Game: {room}</h1>
-                        <h3>Players:</h3>
-                        <ul className="list-group">
-                            {players.map(player => (
-                                <li className="list-group-player" key={player.username} style={(player.username == username) ? {color:"blue"}:{}}>
-                                    {player.username} (score: {player.score})
-                                </li>
-                            ))}
-                        </ul>
-                        <div class="chatArea">
-                            <ul class="messages">
-
-                            </ul>
-                        </div>
-                        <input class="inputMessage" placeholder="Type here..."/>
-                        
-                        {isHost ? (
-                            <div>
-                                <form>
-                                    <input type="checkbox" key="triviaCheck" name="triviaCheck" onChange={() => {checkbox("Trivia")}}/>
-                                    <label htmlFor="triviaCheck"> Trivia</label><br/>
-                                    <input type="checkbox" key="whackCheck" name="whackCheck" onChange={() => {checkbox("Whack")}}/>
-                                    <label htmlFor="whackCheck"> Whack-A-Mole</label><br/>
-                                    <input type="checkbox" key="memoryCheck" name="memoryCheck" onChange={() => {checkbox("Memory")}}/>
-                                    <label htmlFor="memoryCheck"> Memory Cards</label><br/>
-                                    <input type="checkbox" key="snakeCheck" name="snakeCheck" disabled readOnly/>
-                                    <label htmlFor="snakeCheck"> Snake (in development)</label>
-                                    <br/>
-                                </form>
-                                    <br/>
-                                <button style={styles.button} className="btn" onClick={()=>startGame()}>Start Game!</button>
-                            </div>
-                            
-                        ):null}
+                <h1>Game: {room}</h1>
+                <h3>Players:</h3>
+                <ul className="list-group">
+                    {players.map(player => (
+                        <li className="list-group-player" key={player.username} style={(player.username == username) ? {color:"blue"}:{}}>
+                            {player.username} (score: {player.score})
+                        </li>
+                    ))}
+                </ul>
+                <div class="chatArea">
+                    <ul class="messages">
+                        {messages.map(message => {
+                            return (
+                                <li>{message.username}: {message.content}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <form onSubmit={()=>{sendMessage()}}>
+                    <input class="inputMessage" placeholder="Type here..." onChange={handleFormChange}/>
+                    <input type="submit"/>
+                </form>
+                
+                {isHost ? (
+                    <div>
+                        <form>
+                            <input type="checkbox" key="triviaCheck" name="triviaCheck" onChange={() => {checkbox("Trivia")}}/>
+                            <label htmlFor="triviaCheck"> Trivia</label><br/>
+                            <input type="checkbox" key="whackCheck" name="whackCheck" onChange={() => {checkbox("Whack")}}/>
+                            <label htmlFor="whackCheck"> Whack-A-Mole</label><br/>
+                            <input type="checkbox" key="memoryCheck" name="memoryCheck" onChange={() => {checkbox("Memory")}}/>
+                            <label htmlFor="memoryCheck"> Memory Cards</label><br/>
+                            <input type="checkbox" key="snakeCheck" name="snakeCheck" disabled readOnly/>
+                            <label htmlFor="snakeCheck"> Snake (in development)</label>
+                            <br/>
+                        </form>
+                            <br/>
+                        <button style={styles.button} className="btn" onClick={()=>startGame()}>Start Game!</button>
                     </div>
+                    
+                ):null}
+            </div>
         </div>
     )
 }
