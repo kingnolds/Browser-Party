1c0d157d413f78a7f757fa90889df727c66a3e49
Henry Weigand <henryweigand10@gmail.com>
2022-03-16 09:52:12 -0700

    decode

--- src/components/games/Trivia.js
+++ src/components/games/Trivia.js
@@ -32,7 +32,7 @@ function Trivia({socket, room}) {
         //     return decode(an)
         // })
         for (let i = 0; i < ans.length; i++) {
-            const element = decode("&quot");
+            const element = decode(ans[i]);
             ans[i] = element;
         }
         const shuffle = function shuffle(a) {
