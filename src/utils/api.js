e7215be4483117cf1aed8edd045f7f267f22c586
hcweigand10 <henryweigand10@gmail.com>
2022-10-25 13:15:39 -0700

    refactoring, removing CLs

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 // const BASEURL = "http://localhost:3001"
 
 // Deployed
-const BASEURL = "https://tender-ray.cyclic.app"
+const BASEURL = "https://browser-party-backend.onrender.com"
 
 
 const API = {
